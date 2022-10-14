cf9bcac07517eb29b87e4458429403f9215e121d
Saurabh Kedia <kedia.saurabh@gmail.com>
2022-10-14 18:53:24 +0530

    Added subscription endpoint, federation is pending for subscription

--- library-service/src/main/java/learn/kd/graphql/libraryservice/fetcher/BooksFetcher.java
+++ library-service/src/main/java/learn/kd/graphql/libraryservice/fetcher/BooksFetcher.java
@@ -4,13 +4,18 @@ import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.DgsDataFetchingEnvironment;
 import com.netflix.graphql.dgs.DgsEntityFetcher;
+import com.netflix.graphql.dgs.DgsSubscription;
 import learn.kd.generated.schema.DgsConstants;
 import learn.kd.generated.schema.types.Book;
 import learn.kd.generated.schema.types.Student;
 import learn.kd.graphql.libraryservice.loader.BookLoader;
 import lombok.extern.slf4j.Slf4j;
 import org.dataloader.DataLoader;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -29,5 +34,10 @@ public class BooksFetcher {
         DataLoader<String, List<Book>> bookLoader = dfe.getDataLoader(BookLoader.class);
         Student student = dfe.getSource();
         return bookLoader.load(student.getId());
+    }
+
+    @DgsSubscription(field = DgsConstants.SUBSCRIPTION.Ticker)
+    public Publisher<String> publishTicker() {
+        return Flux.interval(Duration.ofSeconds(1)).map(t -> Instant.now().toString());
     }
 }
